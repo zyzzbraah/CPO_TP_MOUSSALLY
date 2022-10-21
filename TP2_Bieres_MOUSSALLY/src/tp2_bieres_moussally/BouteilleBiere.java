056d6872e94fc536d96347816b138b8b1637c577
Léo-Paul Moussally <leo-paul.moussally@epfedu.fr>
2022-10-21 17:05:40 +0200

    FIN du tp2

--- TP2_Bieres_MOUSSALLY/src/tp2_bieres_moussally/BouteilleBiere.java
+++ TP2_Bieres_MOUSSALLY/src/tp2_bieres_moussally/BouteilleBiere.java
@@ -10,15 +10,45 @@ package tp2_bieres_moussally;
  */
 public class BouteilleBiere {
     String nom;
-    String brasserie;
     double degreAlcool;
-    Boolean ouverture;
+    String brasserie;
+    boolean ouverte ;
+    public void lireEtiquette(){
+        System.out.println("Bouteille de "+ nom + "("+degreAlcool+ " degres) \nBrasserie : "+ brasserie);
+    }    
+
            
     
-public void lireEtiquette() {
-    System.out.println("Bouteille de " + nom +" (" + degreAlcool +" degres) \nBrasserie : " + brasserie ) ;
+public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+        nom= unNom;
+        degreAlcool= unDegre;
+        brasserie=uneBrasserie;  
+        ouverte = false;
+    }
+    public boolean Decapsuler(){
+        if (ouverte==false) {
+            ouverte=true;
+            return true;
+        }
+        else {
+            System.out.println("Erreur: la biere est deja ouverte");
+            return false;
+        
+        }
+    }
+    @Override
+public String toString() {
+String chaine_a_retourner;
+chaine_a_retourner = nom + " (" + degreAlcool + " degres) Ouverte ? "; 
+if (ouverte == true ) chaine_a_retourner += "oui" ;
+else chaine_a_retourner += "non" ;
+return chaine_a_retourner ;
 }
+
     
 }
+
+    
+
 
 
--- TP2_Bieres_MOUSSALLY/src/tp2_bieres_moussally/TP2_Bieres_MOUSSALLY.java
+++ TP2_Bieres_MOUSSALLY/src/tp2_bieres_moussally/TP2_Bieres_MOUSSALLY.java
@@ -15,8 +15,41 @@ public class TP2_Bieres_MOUSSALLY {
      */
     public static void main(String[] args) {
         
-        BouteilleBiere UneBierre = new BouteilleBiere
-        // TODO code application logic here
-    }
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvee des Trolls", 7.0, "Dubuisson");
+        
+        uneBiere.ouverte=false;
+        uneBiere.lireEtiquette();
+        
+        BouteilleBiere biere2 = new BouteilleBiere("Leffe", 6.6, "Abbaye de Leffe");
+        
+        biere2.ouverte=false;
+        biere2.lireEtiquette();
+        
+        BouteilleBiere biere3 = new BouteilleBiere ("OURS" , 12.0 , "CHEZ LE JOUSS");
+        biere3.lireEtiquette();
+        BouteilleBiere biere4 = new BouteilleBiere ("TORNADO", 11.5,"PANAM");
+        BouteilleBiere biere5 = new BouteilleBiere ("FEUX FOLLET", 12.5,"BASTAMAN");
+
+        System.out.println(uneBiere.Decapsuler());
+        System.out.println(uneBiere);
+        System.out.println(biere4.Decapsuler());
+        System.out.println(biere4);
+        System.out.println(biere3);
+        }
     
-}
+    }
+
+        
+        
+        
+        
+        
+        
+        
+        
+                
+                
+        // TODO code application logic here
+    
+    
+
