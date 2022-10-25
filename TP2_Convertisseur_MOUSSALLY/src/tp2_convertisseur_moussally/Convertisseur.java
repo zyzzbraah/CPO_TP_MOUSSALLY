533df689b3e2e7afa34f06b1cb69bad1ced28584
Léo-Paul Moussally <leo-paul.moussally@epfedu.fr>
2022-10-25 12:15:03 +0200

    exo 2 tp2 seance 25/10 fin

--- TP2_Convertisseur_MOUSSALLY/src/tp2_convertisseur_moussally/Convertisseur.java
+++ TP2_Convertisseur_MOUSSALLY/src/tp2_convertisseur_moussally/Convertisseur.java
@@ -1,3 +1,5 @@
+package tp2_convertisseur_moussally;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
