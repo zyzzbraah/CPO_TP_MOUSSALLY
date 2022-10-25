dfd2863c4e5833bc00802409c38bbf8ab6b329db
Léo-Paul Moussally <leo-paul.moussally@epfedu.fr>
2022-10-25 10:36:41 +0200

    debut exo 2 tp2 seance 25/10 .2

--- TP2_manip_MOUSSALLY/src/Convertisseur.java
+++ TP2_manip_MOUSSALLY/src/Convertisseur.java
@@ -13,10 +13,24 @@ public class Convertisseur {
     public Convertisseur () {
         nbConversions = 0 ;
     }
-    public double CelsiusVersKelvin (double temp){
+    public double CelsiusVersKelvin (double temp){ //en entrée on donne une temperature temp qui sera converti grace a la ligne suivante
         temp += 273.15 ;
+        return temp;  //en sorti on obtient temp la valeur en entréé convertie a l'unité que l'on souhaite 
+    }
+    public double KelvinVersCelsius (double temp) { //meme chose pour chaques conversions
+        temp -=273.15 ;
         return temp;
     }
-    public double 
+    public double FarenheitVersCelsius (double temp) {
+        temp = (temp-32)*1.8 ; 
+        return temp ;
+    }
+    public double CelsiusVersFarenheit (double temp) {
+        temp = (temp*1.8)-32 ;
+        return temp ;
+    }
+    public double FarenheitVersKelvin (double temp)  {
+        temp = ((temp-32)*1.8)+273,15  ;
+    }
 }
 
