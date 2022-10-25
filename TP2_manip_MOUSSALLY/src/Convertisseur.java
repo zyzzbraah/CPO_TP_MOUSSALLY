0f15dc2a8a1e59cd1304c1d5118b788824ce8ae8
Léo-Paul Moussally <leo-paul.moussally@epfedu.fr>
2022-10-25 10:26:24 +0200

    debut exo 2 tp2 seance 25/10

---
+++ TP2_manip_MOUSSALLY/src/Convertisseur.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author mouss
+ */
+public class Convertisseur {
+    
+    
+    public Convertisseur () {
+        nbConversions = 0 ;
+    }
+    public double CelsiusVersKelvin (double temp){
+        temp += 273.15 ;
+        return temp;
+    }
+    public double 
+}
+
