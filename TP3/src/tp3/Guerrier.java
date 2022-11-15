71c20c45daa2320f04b9c3bfc53683a12d9294a1
Léo-Paul Moussally <leo-paul.moussally@epfedu.fr>
2022-11-15 10:34:46 +0100

    rangement dans des package personnages

---
+++ TP3/src/tp3/Guerrier.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3;
+
+/**
+ *
+ * @author mouss
+ */
+public class Guerrier extends Personnages {
+    boolean cheval;
+
+    public Guerrier(String n , int i , boolean a) {
+        super(n , i );
+        cheval = a;
+    }
+    
+    
+}
