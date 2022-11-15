eeedc4f3ce95aaaeee75761d663d4d7c4be59ac5
Léo-Paul Moussally <leo-paul.moussally@epfedu.fr>
2022-11-15 12:47:50 +0100

    PARTIE 3 jusqua la 34

--- TP3/src/Personnages/Personnages.java
+++ TP3/src/Personnages/Personnages.java
@@ -3,7 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Personnages;
-
+import Armes.*;
+import java.util.ArrayList;
 /**
  *
  * @author mouss
@@ -11,16 +12,42 @@ package Personnages;
 public class Personnages {
     String nom;
     int hp;
+    Armes armePortee= null;
+    ArrayList <Armes> Armes= new ArrayList <>();
     
     public int getHp() {
         return hp;
         
     }
     
+    public void NouvelleArme(Armes weapon){
+
+         if (Armes.size()<=5){
+
+             Armes.add(weapon);
+
+         }
+
+     }
+    
     public void setHp(int hp) {
         this.hp=hp;
     }
      
+    public Armes getArmePortee() {
+
+        return armePortee;
+
+    }
+
+ 
+
+    public void setArmePortee(Armes armePortee) {
+
+        this.armePortee = armePortee;
+
+    }
+    
     public Personnages (String unNom, int nbHp) {
         nom = unNom;
         hp = nbHp;
@@ -33,10 +60,20 @@ public class Personnages {
         
     }
     
-    @Override
+     @Override
+
     public String toString() {
-        String rep;
-        rep = nom + hp +"pv";
-        return rep;
+
+        if (armePortee!=null){
+
+            return "Personnages{" + "nom=" + nom + ", vie=" + hp + ", Arme portee : "+ armePortee + '}';
+
+        }
+
+        return "Personnages{" + "nom=" + nom + ", vie=" + hp + '}';
+
     }
+
+   
+
 }
